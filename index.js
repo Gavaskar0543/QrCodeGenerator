b14efee063425cae41c436c3640733a09f9b50d5
Gavaskar <gavaskaraeg@gmail.com>
2023-11-08 11:11:05 +0000

    project completed

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
+const btn = document.querySelector('.submitBtn');
+const downloadButton = document.getElementById('download-button'); // Add a download button
 
+btn.onclick = () => {
+    const urlBox = document.querySelector('.urlBox').value; // Move this inside the click event handler
+    const qrCodeDiv = document.getElementById('qrCode');
 
-
-
-const btn = document.getElementById('submitBtn');
-let urlBox = document.getElementById('urlBox').value;
-console.log(urlBox);
-
-btn.onclick = () =>{
-
-    if(urlBox === ''){
-        btn.classList.add('liveAlertBtn');
-        return;
+    if (urlBox === '') {
+        alert('Please enter some text or a URL to generate a QR code.');
     }
-    
+
+
+    qrCodeDiv.innerHTML = '';
+    const qrcode = new QRCode(qrCodeDiv, {
+        text: urlBox,
+        width: 128,
+        height: 128,
+
+        
+    });
+    downloadButton.style.display = 'block';
+
+    downloadButton.addEventListener('click', function () {
+        const canvas = qrCodeDiv.querySelector('canvas');
+        if (canvas) {
+            const url = canvas.toDataURL('image/png');
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'qrcode.png';
+            link.click();
+        }
+    });
 }
 
 
@@ -23,23 +40,3 @@ btn.onclick = () =>{
 
 
 
-//alert
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-const alertTrigger = document.querySelector('.liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Nice, you triggered this alert message!', 'success')
-  })
-}
